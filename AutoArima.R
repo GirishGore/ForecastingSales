62d1a1c989169484eb452c71628c49f08e48602e
Girish Gore <girish.gore@orchestro.com>
2015-11-08 20:32:53 +0530

    NaiveLastYear

--- AutoArima.R
+++ AutoArima.R
@@ -4,7 +4,8 @@ forecasting.algorithm <- function (train , test , algoname , ...) {
   
   ALGONAMES <- c('Using.auto.arima',
                  'Visualizing.TimeSeries.Decomposition',
-                 'Using.furier.series');
+                 'Using.furier.series',
+                 'Using.Classiscal.Approach');
   
   if(algoname %in% ALGONAMES){
      cat(" This algorithm supported ")
@@ -26,8 +27,8 @@ forecasting.algorithm <- function (train , test , algoname , ...) {
   
   for(currentStore in forecastStores)
   {
-    if (currentStore == 512){
-    #currentStore <- 512
+    #if (currentStore == 512){
+    #currentStore <- 511
     cat("Running Forecasting for : ", currentStore , "\n")
     currentData <- filter(train , train$Store == currentStore)
     testData <- filter(test , test$Store == currentStore)
@@ -35,9 +36,12 @@ forecasting.algorithm <- function (train , test , algoname , ...) {
     cat("Running for store ", currentStore , " has data with rows ", nrow(currentData))
     s <- ts(currentData$Sales, frequency=365)
     
+    salesFcst <- filter(currentData, Date >= '2014-08-02' & Date <= '2014-09-18')
+    
+    
     newFcst <- f(currentData, testData , currentStore , s)
     myfcst <- bind_rows( myfcst, newFcst)
-    }
+    #}
   }
   
   myfcst   
@@ -82,20 +86,43 @@ Using.furier.series <- function(currentData, testData, currentStore , s)
 {
   horizon <- 365
   k <- length(s)/horizon
-  s <- window(s ,k * horizon)
+  ?window
+  s <- window(s ,1,k * horizon)
   cat(" Horizon inferred ",horizon , " length of time sereies s ", length(s))
   
-  model <- auto.arima(s,xreg = data.frame(as.numeric(currentData$Promo), fourier(s,K=k)))
+  model <- auto.arima(s,xreg = data.frame(as.numeric(currentData$Promo), fourier(s,K=k)), seasonal = FALSE)
   
   cat(" Finished building a model ")
   fc <- predict(model, h=horizon , 
                 newxreg= data.frame(as.numeric(testData$Promo), fourier(testData$Promo,K=k )))
   
   
-  newFcst <- data.frame(Store = rep(currentStore,horion),
+  newFcst <- data.frame(Store = rep(currentStore,horizon),
                         Date = seq(as.Date("2015-08-01"), as.Date("2015-09-17"), "day"),
                         Sales = fc$pred)
   
   newFcst
   
+}
+
+Using.Classiscal.Approach <- function(currentData, testData , currentStore ,s)
+{
+  horizon <- 48
+  salesFcst <- filter(currentData, Date >= '2014-08-02' & Date <= '2014-09-18')
+  
+  if(length(salesFcst) < 48)
+  {
+  newFcst <- data.frame(Store = rep(currentStore,horizon),
+                        Date = seq(as.Date("2015-08-01"), as.Date("2015-09-17"), "day"),
+                        Sales = rep(5000,48))
+    
+  }
+  else
+  {
+    newFcst <- data.frame(Store = rep(currentStore,horizon),
+                          Date = seq(as.Date("2015-08-01"), as.Date("2015-09-17"), "day"),
+                          Sales = salesFcst$Sales)
+    
+  }
+  newFcst
 }
--- preProcess.R
+++ preProcess.R
@@ -33,10 +33,10 @@ test$SchoolHoliday <- as.factor(test$SchoolHoliday)
 library(dplyr)
 train <- arrange(train, Store, Date)
 test <- arrange(test ,  Store ,Date)
-train.frame <- forecasting.algorithm(train, test , 'Using.furier.series')
+train.frame <- forecasting.algorithm(train, test , 'Using.Classiscal.Approach')
 
 submission <- merge(test , train.frame , by = c("Store","Date"))
 submission <- submission[,c("Id","Sales")]
 submission <- arrange(submission, Id)
 
-write.csv(submission,"F1Submission.csv", row.names = FALSE)
+write.csv(submission,"NaiveLastYear.csv", row.names = FALSE)
